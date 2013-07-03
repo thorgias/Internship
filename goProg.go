984ebaf89ecdb226aff77dfb9e3f5c91ec9d6a4f
Cody <cody@ubuntu.(none)>
2013-07-02 21:56:09 -0400

    committing my code

--- goProg.go
+++ goProg.go
@@ -1,8 +1,43 @@
+
 package main
 
-import "fmt"
 
-func main() {
-	fmt.Printf("Hello world!")
+import "fmt"
+import "tweetstream"
+/**import "tweetstream"
+import "re"
+import "requests"
+import "json"
+import "simplejson"
+import "sys"
+import "daemon"
+*/
+/**from "pymongo" import "MongoClient"
+
+from itertools import chain
+from collections import OrderedDict
+from nltk.corpus import stopwords
+from tweetstream import ConnectionError
+*/
+
+
+func twitterStream(consumer_key string, consumer_secret string, access_token string, access_secret string) (track []int) {
+	//for idx, val := range in {
+	    //for tweet, val := stream {}
+	var flag1 bool = true
+	for _, tweet :=range stream
+		if tweet, err := conn.Next(); err == nil {
+				source_platform = tweet ['source']
+				flag1 = false
+
+}       else if flag1 := true
+
+	else {
+				source_platform = tweet['source'].split('"')[4].split('>')[1].split('<')[0]
+}
+         tweet_time = tweet['created_at']
+
+	func main() {
+	fmt.Printf("Go Program")
 }
 
